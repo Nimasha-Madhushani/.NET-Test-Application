513729e7ad57959f5a3a40ea346768cc01cb46dd
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-12-16 15:30:13 +0530

    initiate project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//namespace
+namespace TestProject1
+{
+    //Main Class
+    class Tests
+    {
+        //Entry Point Method
+        static void Main(string[] args)
+        {
+            //start here//
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
